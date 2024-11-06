253960232f8fc34bfba156b3b66c1a49877265b8
Bezyl Mophat Otieno <bezyl.otieno@griffinglobaltech.com>
2024-11-06 07:41:31 +0300

    Updated the listened for events

--- lib/db/webhooks.ts
+++ lib/db/webhooks.ts
@@ -1,27 +1,27 @@
-import { headers } from 'next/headers';
-import {UserWebhookEvent, WebhookEvent} from '@clerk/nextjs/server';
-import { db } from './index'
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { db } from './index';
 
-export async function createOrUpdateUser(event: UserWebhookEvent) {
+export async function createOrUpdateUser(event: WebhookEvent) {
     // @ts-ignore
-    const { id, primary_email_address_id, created_at, updated_at } = event.data;
+    const { id, email_addresses, primary_email_address_id,created_at, updated_at } = event.data;
 
+    const primaryEmail = email_addresses.find((email: { id: any, email_address:string }) => email.id === primary_email_address_id);
 
-    if (!primary_email_address_id) {
+    if (!primaryEmail) {
         throw new Error('User must have a primary email address');
     }
 
     try {
         const result = await db.query(
             `INSERT INTO DebitUsers (user_id, email, created_at, updated_at)
-       VALUES ($1, $2, $3, $4)
-       ON CONFLICT (user_id) DO UPDATE
-       SET email = $2,
-           updated_at = $4
-       RETURNING *`,
+             VALUES ($1, $2, $3, $4)
+                 ON CONFLICT (user_id) DO UPDATE
+                                              SET email = $2,
+                                              updated_at = $4
+                                              RETURNING *`,
             [
                 id,
-                primary_email_address_id,
+                primaryEmail.email_address,
                 new Date(created_at),
                 new Date(updated_at)
             ]
